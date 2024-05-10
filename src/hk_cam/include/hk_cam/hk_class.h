088eaaaad478cdaf1dc32b446dced69d4db285a6
yjrqz777 <3210551161@qq.com>
2024-05-10 19:45:08 +0800

    多线程发布图像

---
+++ src/hk_cam/include/hk_cam/hk_class.h
@@ -0,0 +1,8 @@
+//
+// Created by yjrqz on 2024/5/10.
+//
+
+#ifndef BUILD_HK_CLASS_H
+#define BUILD_HK_CLASS_H
+
+#endif //BUILD_HK_CLASS_H
---
+++ src/hk_cam/src/hk_class.cpp
@@ -0,0 +1,3 @@
+//
+// Created by yjrqz on 2024/5/10.
+//
